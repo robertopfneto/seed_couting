c802c2e1f4e858850e5d7b01df57957ce385473b
robertopfneto <neto2014roberto@gmail.com>
2025-06-19 15:48:51 -0300

    - Conversão das imagens para escala cinza
    - Aplicacao do CLAHE
    
    - Redimensionamento das imanges
    
    - Preenchimento com padding

--- pre-processing.py
+++ pre-processing.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import os
 import pandas as pd
@@ -12,4 +12,38 @@ Transformações realizadas:
 
 """
 def preprocess_image(image_path, annotation_path, output_dir, target_size=(512,512)):
-    return
+    os.makedirs(output_dir, exist_ok=True)
+    basename = os.path.splitext(os.path.basename(image_path))[0]
+    original_filename = os.path.basename(image_path)
+
+    # convertendo as imagens para escala cinza
+    img = cv.imread(image_path)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # aplicando CLAHE (melhora de contraste)
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    out_img = clahe.apply(gray)
+
+    # redimensionando
+    height, widht = out_img.shape #pegando dimensoes
+    scale = min(target_size[0] / height, target_size[1] / widht) #definindo scala
+    new_wid = int(widht * scale)
+    new_heig = int(height * scale)
+    resized_img = cv.resize(out_img, (new_wid,new_heig), interpolation=cv.INTER_AREA) #interpolation torna a redução de resolução mais suave
+
+    # preenchendo as bordas com padding
+    delta_heig = target_size[0] - new_heig
+    delta_wid = target_size[1] - new_wid
+    
+    top = delta_heig // 2
+    bottom = delta_heig - top
+
+    left = delta_wid // 2
+    right = delta_wid - left
+
+    padded = cv.copyMakeBorder(resized_img, top, bottom, left, right, cv.BORDER_CONSTANT, value=0)
+
+    # ajustando anotacoes
+    annotations = pd.read_csv(annotation_path, header=None, names=["label", "x", "y", "filename", "width", "height"])
+    img_ann = annotations[annotations["filename"] == original_filename].copy()
+    img_ann["x"] = img_ann["x"] * scale + left
